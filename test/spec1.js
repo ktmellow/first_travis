63443986544f5e7702b6e73071b28e63776f9017
ktmellow <letsspamkt@gmail.com>
2016-07-29 14:24:42 -0700

    changed travis yaml file

--- test/spec1.js
+++ test/spec1.js
@@ -4,7 +4,7 @@ describe("Addition", function(){
   it("adds 1 and 1", function(){
     expect(1+1).to.equal(2)
   });
-  // it("fails because 1+1 is not 3", function(){
-  //   expect(1+1).to.equal(2)
-  // });
+  it("fails because 1+1 is not 3", function(){
+    expect(1+1).to.equal(2)
+  });
 });
